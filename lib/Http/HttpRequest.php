2e71d4cb734a4e1b1775501b7918d5ded454dac0
François Kooman <fkooman@tuxed.net>
2012-08-03 12:31:53 +0200

    add initial remoteStorage api functionality

--- lib/Http/HttpRequest.php
+++ lib/Http/HttpRequest.php
@@ -142,6 +142,10 @@ class HttpRequest {
     public function getRestInfo() {
         return new RestInfo($this->getPathInfo(), $this->getRequestMethod());
     }
+    
+    public function remoteStorageRestInfo() {
+        return new RemoteStorageRestInfo($this->getPathInfo(), $this->getRequestMethod());
+    }
 
     public function getBasicAuthUser() {
         return $this->headerExists("PHP_AUTH_USER") ? $this->getHeader("PHP_AUTH_USER") : NULL;
@@ -149,6 +153,63 @@ class HttpRequest {
 
     public function getBasicAuthPass() {
         return $this->headerExists("PHP_AUTH_PW") ? $this->getHeader("PHP_AUTH_PW") : NULL;
+    }
+
+}
+
+class RemoteStorageRestInfo {
+
+    private $_pathInfo;
+    private $_requestMethod;
+    private $_explodedPath;
+
+    public function __construct($pathInfo, $requestMethod) {
+        $this->_pathInfo = $pathInfo;
+        $this->_requestMethod = $requestMethod;
+        $this->_explodedPath = NULL;
+
+        if(NULL !== $this->_pathInfo) {
+            if(1 < strlen($this->_pathInfo)) {
+                $this->_explodedPath = explode("/", substr($this->_pathInfo, 1));
+            }
+        }
+    }
+
+    public function getCollection() {
+        if(is_array($this->_explodedPath) && 0 < count($this->_explodedPath)) {
+            return $this->_explodedPath[0];
+        }
+        return NULL;
+    }
+
+    public function getPathInfo() {
+        return $this->_pathInfo;
+    }
+
+    public function getRequestMethod() {
+        return $this->_requestMethod;
+    }
+
+    public function getResourceOwner() {
+        if(is_array($this->_explodedPath) && 0 < count($this->_explodedPath)) {
+            return $this->_explodedPath[0];
+        }
+        return NULL;
+    }
+
+    public function isDirectoryRequest() {
+        return empty($this->_explodedPath[count($this->_explodedPath)-1]);
+    }
+
+    public function isResourceRequest() {
+        return !$this->isDirectory();
+    }
+
+    public function isPublicRequest() {
+        if(is_array($this->_explodedPath) && 1 < count($this->_explodedPath)) {
+            return $this->_explodedPath[1] === "public";
+        }
+        return FALSE;
     }
 
 }
---
+++ www/api.php
@@ -0,0 +1,165 @@
+<?php
+
+require_once "../lib/Config.php";
+require_once "../lib/Http/Uri.php";
+require_once "../lib/Http/HttpRequest.php";
+require_once "../lib/Http/HttpResponse.php";
+require_once "../lib/Http/IncomingHttpRequest.php";
+require_once "../lib/OAuth/RemoteResourceServer.php";
+
+$response = new HttpResponse();
+$response->setHeader("Content-Type", "application/json");
+$response->setHeader("Access-Control-Allow-Origin", "*");
+
+try { 
+    $config = new Config(dirname(__DIR__) . DIRECTORY_SEPARATOR . "config" . DIRECTORY_SEPARATOR . "remoteStorage.ini");
+
+    $rootDirectory = $this->_s->getValue('filesDirectory')
+
+    $incomingRequest = new IncomingHttpRequest();
+    $request = $incomingRequest->getRequest();
+
+    $restInfo = $request->getRemoteStorageRestInfo();
+    
+    $rs = new RemoteResourceServer($config->getValue("oauthTokenEndpoint"));
+
+    if($restInfo->isPublicRequest() || $request->headerExists("HTTP_AUTHORIZATION")) { 
+        // FIXME: a bit ugly to deal with public requests...
+        $token = $restInfo->isPublicRequest() ? NULL : $rs->verify($request->getHeader("HTTP_AUTHORIZATION"));
+
+        // handle API
+        switch($restInfo->getRequestMethod()) {
+
+            case "GET":
+                $ro = $restInfo->getResourceOwner();
+
+                if($restInfo->isPublicRequest()) {
+                    if($restInfo->isDirectoryRequest()) {
+                        throw new RemoteStorageException("invalid_request", "not allowed to list contents of public folder");
+                    }
+                    // public but not listing, return file if it exists...
+                    $file = realpath($rootDirectory . $restInfo->getPathInfo());
+                    if(FALSE === $file || !is_file($file)) {
+                        throw new RemoteStorageException("not_found", "the file was not found");
+                    }
+                    $mimeType = xattr_get($file, 'mime_type');
+                    $response->setHeader("Content-Type", $mimeType);
+                    $response->setContent(file_get_contents($file));
+                } else {
+                    if($ro !== $token['resource_owner_id']) {
+                        throw new RemoteStorageException("invalid_request", "you are not allowed to access files not belonging to you or to a public directory");
+                    }
+
+                    // verify scope
+                    $c = $restInfo->getCollection();
+                    $scope = explode(" ", $token->scope);
+                    if(!in_array($c . ":r", $scope) && !in_array($c . ":rw", $scope)) {
+                        throw new RemoteStorageException("invalid_request", "insufficient scope");
+                    }
+
+                    if($restInfo->isDirectoryRequest()) {
+                        // return directory listing
+                        $dir = realpath($rootDirectory . $restInfo->getPathInfo());
+                        if(FALSE === $dir || !is_directory($dir)) {
+                            throw new RemoteStorageException("not_found", "the directory was not found");
+                        }
+                        $entries = array();
+                        foreach(glob($dir . DIRECTORY_SEPARATOR . "*", GLOB_MARK) as $e) {
+                            $entries[basename($e)] = filemtime($e);
+                        }
+                        $response->setContent(json_encode($entries));
+                    }
+                    // accessing your own file, go ahead, return file if it exists...
+                }
+                break;
+    
+            case "PUT":
+                $ro = $restInfo->getResourceOwner();
+                if($ro !== $token['resource_owner_id']) {
+                    throw new RemoteStorageException("invalid_request", "you are not allowed to write files to a location not belonging to you");
+                }
+
+                $userDirectory = $rootDirectory . DIRECTORY_SEPARATOR . $ro;
+                createDirectories(array($rootDirectory, $userDirectory));
+
+                // verify scope
+                $c = $restInfo->getCollection();
+                $scope = explode(" ", $token->scope);
+                if(!in_array($c . ":rw", $scope)) {
+                    throw new RemoteStorageException("invalid_request", "insufficient scope");
+                }
+
+                if($restInfo->isDirectoryRequest()) {
+                    // create the directory
+                    $newDirectory = $rootDirectory . $restInfo->getPathInfo();
+                    createDirectories(array($newDirectory));
+                } else {
+                    // upload a file
+                    $file = realpath($rootDirectory . $restInfo->getPathInfo()); 
+                    $dir = dirname($file);
+                    if(FALSE === $dir || !is_directory($dir)) {
+                        throw new RemoteStorageException("not_found", "the directory was not found");
+                    }
+
+                    $contentType = $request->getHeader("Content-Type");
+                    file_put_contents($file, $request->getContent());
+                    // also store the accompanying mime type in the file system extended attribute
+                    xattr_set($file, 'mime_type', $contentType);
+                }   
+                break;
+
+            case "DELETE":
+
+
+                break;
+
+            case "OPTIONS":
+
+                break;
+
+            default:
+                // ...
+                break;
+
+        }
+
+    } else {
+        $response->setStatusCode(401);
+        $response->setHeader("WWW-Authenticate", sprintf('Bearer realm="Resource Server"'));
+        $response->setContent(json_encode(array("error"=> "not_authorized", "error_description" => "need authorization to access this service")));
+    }   
+} catch (Exception $e) {
+    switch(get_class($e)) {
+        case "VerifyException":
+            $response->setStatusCode($e->getResponseCode());
+            $response->setHeader("WWW-Authenticate", sprintf('Bearer realm="Resource Server",error="%s",error_description="%s"', $e->getMessage(), $e->getDescription()));
+            $response->setContent(json_encode(array("error" => $e->getMessage(), "error_description" => $e->getDescription())));
+            break;
+
+        case "RemoteStorageException":
+            $response->setStatusCode($e->getResponseCode());
+            $response->setContent(json_encode(array("error" => $e->getMessage(), "error_description" => $e->getDescription())));
+            break;
+
+        default:
+            // any other error thrown by any of the modules, assume internal server error
+            $response->setStatusCode(500);
+            $response->setContent(json_encode(array("error" => "internal_server_error", "error_description" => $e->getMessage())));
+            break;
+    }
+
+}
+
+$response->sendResponse();
+
+function createDirectories(array $directories) { 
+    foreach($directories as $d) { 
+        if(!file_exists($d)) {
+            if (@mkdir($d, 0775, TRUE) === FALSE) {
+                throw new RemoteStorageException("error", "unable to create directory");
+            }
+        }
+    }
+}
+
+?>
